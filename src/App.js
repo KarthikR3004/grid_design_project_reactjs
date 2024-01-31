8206173fd29f7e6d958164c237e8b505e19f3aa8
KarthikR3004 <karthikram3004@gmail.com>
2024-01-31 22:20:01 +0530

    grid design project using reactjs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Cards from './cards';
+import Topbar from './header';
+import Main from './main';
+
+function App() {
+  return (
+      <div >
+        <Topbar/>
+        <Main/>
+        <Cards/>
+      </div>
+  );
+}
+
+export default App;
+
